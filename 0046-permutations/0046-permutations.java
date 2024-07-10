3c881cd77929e6c142a13a0096244eb8816bad0f
Jigyansu Nanda <62047889+jigyansunanda@users.noreply.github.com>
2024-07-10 06:48:35 +0530

    Time: 1 ms (93.44%), Space: 44.5 MB (36.85%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,34 @@
+class Solution {
+    private List<Integer> permutation;
+    private List<List<Integer>> permutations;
+
+    public List<List<Integer>> permute(int[] arr) {
+        int n = arr.length;
+        this.permutation = new ArrayList<>();
+        this.permutations = new ArrayList<>();
+        dfs(arr, 0);
+        return permutations;
+    }
+
+    private void dfs(int[] arr, int currentIndex) {
+        if (currentIndex == arr.length) {
+            permutations.add(new ArrayList<>(permutation));
+            return;
+        }
+        int indexToBeSwapped = currentIndex;
+        while (indexToBeSwapped < arr.length) {
+            swap(arr, indexToBeSwapped, currentIndex);
+            permutation.add(arr[currentIndex]);
+            dfs(arr, currentIndex + 1);
+            permutation.remove(permutation.size() - 1);
+            swap(arr, indexToBeSwapped, currentIndex);
+            ++indexToBeSwapped;
+        }
+    }
+
+    private void swap(int[] arr, int index1, int index2) {
+        int tmp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = tmp;
+    }
+}
